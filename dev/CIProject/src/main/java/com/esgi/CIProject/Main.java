2fa3ed4b677e918908d36a9131b272846d6f71ad
MikeLebeauSio <mike.lebeau.sio@gmail.com>
2018-01-17 16:20:44 +0100

    Add Nexus folder

---
+++ dev/CIProject/src/main/java/com/esgi/CIProject/Main.java
@@ -0,0 +1,12 @@
+package com.esgi.CIProject;
+
+/**
+ * Created by LEBEAU Mike
+ * ESGI - 4 AL 1
+ * on 17/01/2018
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world bitch !");
+    }
+}
